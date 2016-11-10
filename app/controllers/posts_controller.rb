2440e9c6d8cbe30cab329964a607c2091560c2a0
qinfeng <q884822@gmail.com>
2016-11-10 14:43:26 +0800

    建立posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: 'Update Success'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    @post.destroy
+    redirect_to posts_path, alert: 'Post deleted'
+  end
+
+  private
+
+
+
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
+end
