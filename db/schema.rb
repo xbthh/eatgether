1f92ca910b63cdae8536f3adbc2c56dd102b3a78
xbthh <hanhan_smith@hotmail.com>
2016-11-10 15:53:32 +0800

    建立 order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110074528) do
+ActiveRecord::Schema.define(version: 20161110075018) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "post_1_id"
+    t.integer  "post_2_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
