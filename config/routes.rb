ddfc9de0511e6932bcd80a519a36e38514003740
qinfeng <q884822@gmail.com>
2016-11-10 16:02:23 +0800

    change some

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   resources :posts
-end
-=======
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   end
->>>>>>> d1889f19acfbc174685326ceb169ef57be5132a8
